ae996e60903aabcf19acd06564ee84467e243266
Alex H <luis.gamino@ue-germany.de>
2024-04-26 14:44:52 +0200

    enable the admin to manipulate categories and subcategories

--- announcement_system/homepage/admin.py
+++ announcement_system/homepage/admin.py
@@ -1,5 +1,9 @@
 # homepage/admin.py
 
 from django.contrib import admin
+from .models import Category, Subcategory
 
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Subcategory)
